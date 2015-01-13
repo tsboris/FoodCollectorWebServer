b63cc504bbf8975f07b69549fe6578bdf910ca02
ingrid <isolovan@gmail.com>
2015-01-13 17:48:32 +0200

    Register user controller

--- app/controllers/registered_user_for_publications_controller.rb
+++ app/controllers/registered_user_for_publications_controller.rb
@@ -6,7 +6,6 @@ class RegisteredUserForPublicationsController < ApplicationController
     render json: registered_user_for_publication
   rescue
     render json: registered_user_for_publication.errors, status: :unprocessable_entity 
-    #render :json => {:error => "cannot create record"}.to_json, :status => 400
   end
 
   def index
@@ -16,7 +15,7 @@ class RegisteredUserForPublicationsController < ApplicationController
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_registered_user_for_publication
-    #@registered_user_for_publication = RegisteredUserForPublication.find(params[:id])
+    @registered_user_for_publication = RegisteredUserForPublication.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
